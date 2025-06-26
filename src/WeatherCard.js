5d3e87d4d6941339da4079a08108d8bd921353c0
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-06-26 21:47:10 +0530

    Create WeatherCard.js

---
+++ src/WeatherCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const WeatherCard = ({ data }) => {
+  return (
+    <div style={{ marginTop: 20 }}>
+      <h2>{data.name}</h2>
+      <p>{data.weather[0].description}</p>
+      <p>🌡️ Temp: {data.main.temp} °C</p>
+      <p>💧 Humidity: {data.main.humidity}%</p>
+      <p>🌬️ Wind: {data.wind.speed} m/s</p>
+    </div>
+  );
+};
+
+export default WeatherCard;
